b641b20922ef1e48331a59cdb3977b687d69bda1
CLQuantizer <ezioxxk@gmail.com>
2022-06-03 00:25:31 +0100

    newly adjusted

---
+++ backend/users/database.py
@@ -0,0 +1,97 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker, Session
+from . import models, schemas
+
+
+SQLALCHEMY_DATABASE_URL = "postgresql://ezio:password@localhost:5432/ezio"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+Base = declarative_base()
+
+# the CRUD operations
+def get_user(db: Session, user_id: int):
+    return db.query(models.User).filter(models.User.id == user_id).first()
+
+def get_user_by_username(db: Session, username: str):
+    return db.query(models.User).filter(models.User.username == username).first()
+
+def get_users(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.User).offset(skip).limit(limit).all()
+
+def create_user(db: Session, user: schemas.UserCreate):
+    hashed_password = user.password+"123"
+    new_user = models.User(username=user.username, hashed_password=hashed_password)
+    db.add(new_user)
+    db.commit()
+    db.refresh(new_user)
+    return new_user
+
+def delete_user_by_id(db: Session, user_id:int):
+    old_user = get_user(db=db,user_id=user_id)
+    db.delete(old_user)
+    db.commit()
+    return old_user
+
+def delete_user_by_username(db: Session, username:str):
+    old_user = get_user_by_username(db=db,username=username)
+    db.delete(old_user)
+    db.commit()
+    return old_user
+
+def get_journals(db: Session, skip: int = 0, limit: int = 100):
+    return db.query(models.Journal).offset(skip).limit(limit).all()
+
+def create_user_journal(db: Session, journal: schemas.JournalCreate, user_id: int):
+    new_journal = models.Journal(**journal.dict(), author_id=user_id)
+    db.add(new_journal)
+    db.commit()
+    db.refresh(new_journal)
+    return new_journal
+
+# async def get_journals():
+#     journals = []
+#     async for journal in journal_collection.find():
+#         journals.append(journal_helper(journal))
+#     return journals
+
+# async def add_journal(journal_data:dict)->dict:
+#     journal = await journal_collection.insert_one(journal_data)
+#     new_journal = await journal_collection.find_one({"_id":journal.inserted_id})
+#     return journal_helper(new_journal)
+
+# async def get_journal(id:str)->dict:
+#     journal = await journal_collection.find_one({"_id":ObjectId(id)})
+#     if journal:
+#         return journal_helper(journal)
+
+# async def update_journal(id:str, data:dict):
+
+#     if len(data)<1:
+#         return False
+#     journal = await journal_collection.find_one({"_id":ObjectId(id)})
+#     if journal:
+#         updated_journal = await journal_collection.update_one({"_id":ObjectId(id)},{"$set":data})
+#         print(update_journal)
+#     if update_journal:
+#         return True
+#     return False
+
+# async def delete_journal(id:str):
+#     journal = await journal_collection.find_one({"_id":ObjectId(id)})
+#     if journal:
+#         await journal_collection.delete_one({"_id":ObjectId(id)})
+#         return True
+
+# # helpers
+
+# def journal_helper(journal) -> dict:
+#     return {
+#         "id": str(journal["_id"]),
+#         "title": journal["title"],
+#         "author": journal["author"],
+#         "date": journal["date"],
+#         "body": journal["body"],
+#     }
---
+++ backend/users/router.py
@@ -0,0 +1,69 @@
+from fastapi import APIRouter, Body, Depends, FastAPI, HTTPException
+from sqlalchemy.orm import Session
+from . import models, schemas
+from .database import(
+    SessionLocal,
+    engine,
+    get_user,
+    get_user_by_username,
+    get_users,
+    create_user,
+    delete_user_by_id,
+    delete_user_by_username,
+    get_journals,
+    create_user_journal,
+)
+
+
+models.Base.metadata.create_all(bind=engine)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+userRouter = APIRouter()
+
+@userRouter.post("/",  response_model=schemas.User)
+def add_new_user(user: schemas.UserCreate, db: Session=Depends(get_db)):
+    db_user = get_user_by_username(db, username=user.username)
+    if db_user:
+        raise HTTPException(status_code=400, detail="username already registered")
+    return create_user(db=db, user=user)
+
+@userRouter.get("/", response_model=list[schemas.User], response_description="All users data")
+def read_users_data(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    users = get_users(db, skip=skip,limit=limit)
+    return users
+
+@userRouter.get("/{user_id}", response_model=schemas.User,response_description="Single user data retrieved")
+def read_user_data(user_id: int, db: Session = Depends(get_db)):
+    db_user = get_user(db, user_id=user_id)
+    if db_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return db_user
+
+@userRouter.delete("/{user_id}", response_model=schemas.User,response_description="Single user data retrieved")
+def delete_user_data_by_id(user_id: int, db: Session = Depends(get_db)):
+    old_user = get_user(db, user_id=user_id)
+    if old_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return delete_user_by_id(db=db, user_id=user_id)
+
+@userRouter.delete("/", response_model=schemas.User,response_description="Single user data retrieved")
+def delete_user_data_by_name(user: schemas.UserBase, db: Session = Depends(get_db)):
+    old_user = get_user_by_username(db, username=user.username)
+    if old_user is None:
+        raise HTTPException(status_code=404, detail="User not found")
+    return delete_user_by_username(db=db, username=user.username)
+
+@userRouter.post("/{user_id}/journals/", response_model=schemas.Journal, response_description="create a journal for a user")
+def add_new_journal_for_user(user_id: int, journal: schemas.JournalCreate, db: Session = Depends(get_db)):
+    return create_user_journal(db=db, journal=journal, user_id=user_id)
+
+@userRouter.get("/journals/", response_model=list[schemas.Journal], response_description="get all journals")
+def read_journals_data(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+    journals = get_journals(db, skip=skip, limit=limit)
+    return journals
