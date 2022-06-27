c44d563cd0848854f7d5fc822483f9cbfc6b4200
CLQuantizer <ezioxxk@gmail.com>
2022-06-27 20:38:51 +0100

    the operations here should be atomic

--- backend/users/database.py
+++ backend/users/database.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 from jose import JWTError, jwt
 from passlib.context import CryptContext
+from fastapi import HTTPException
 from . import models, schemas
 
 SQLALCHEMY_DATABASE_URL = "postgresql://ezio:password@localhost:5432/ezio"
@@ -18,6 +19,7 @@ def get_password_hash(password):
     return pwd_context.hash(password)
 
 # the CRUD operations
+# they should all be atomic operations
 def create_user(db: Session, user: schemas.UserCreate):
     hashed_password = get_password_hash(user.password)
     new_user = models.User(username=user.username, hashed_password=hashed_password)
@@ -32,19 +34,29 @@ def get_user_by_username(db: Session, username: str):
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.User).offset(skip).limit(limit).all()
 
-def delete_user_by_username(db: Session, username:str):
-    old_user = get_user_by_username(db=db,username=username)
+def delete_user_by_username(db: Session, user: schemas.UserBase):
+    old_user = get_user_by_username(db=db,username=user.username)
     db.delete(old_user)
     db.commit()
     return old_user
 
-def create_user_journal(db: Session, journal: schemas.JournalCreate, username: str):
-    user = get_user_by_username(db=db, username=username)
+def create_user_journal(db: Session, journal: schemas.JournalCreate, user: schemas.UserBase):
+    user = get_user_by_username(db=db, username=user.username)
+    new_journal = db.query(models.Journal).filter(models.Journal.title == journal.title).first()
+    if new_journal:
+        raise HTTPException(status_code=400, detail="Journal already exists")
+        return None
     new_journal = models.Journal(**journal.dict(), author_id=user.id)
     db.add(new_journal)
     db.commit()
     db.refresh(new_journal)
     return new_journal
+
+def update_journal(db: Session, journal: schemas.JournalUpdate):
+    db.query(models.Journal).filter(models.Journal.title == journal.title).update(journal.dict())
+    db.commit()
+    db.refresh(journal)
+    return journal
 
 def get_user_journals(db: Session, username: str):
     user = get_user_by_username(db=db, username=username)
@@ -57,7 +69,6 @@ def get_journal_by_title(db: Session, title: str):
     return db.query(models.Journal).filter(models.Journal.title == title).first()
 
 def delete_journal_by_title(db: Session, title: str):
-    journal = get_journal_by_title(db=db, title=title)
-    db.delete(journal)
+    journal = db.query(models.Journal).filter(models.Journal.title == title).delete()
     db.commit()
     return journal
