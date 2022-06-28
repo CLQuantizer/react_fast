5728bd06efc87ad48bbd5fdf215793602da5a1bd
CLQuantizer <ezioxxk@gmail.com>
2022-06-28 10:04:01 +0100

    truly abstracted deployment logic

--- frontend/src/Config.js
+++ frontend/src/Config.js
@@ -1,10 +1,17 @@
 class Config {
-  constructor() {
-    this.developmentServer = 'http://localhost:3000/';
-    this.productionServer = 'http://langedev.net/';
-    this.developmentApi = 'http://localhost:8000/';
-    this.productionApi = 'http://langedev.net:8000/';
+  constructor(mode) {
+    this.mode = mode;
+    if (this.mode === 'development') {
+      this.server = 'http://localhost:3000/';
+      this.api = 'http://localhost:8000/';
+    }else if (this.mode === 'production') {
+      this.server = 'http://langedev.net/'
+      this.api = 'http://langedev.net:8000/';
+    }else{
+      throw new Error('mode must be either \'development\' or \'production\'');
+    }
   }
 }
 
-export default new Config();
+export default new Config('development');
+// export default new Config('production');
